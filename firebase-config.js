825ab57eda59c11d75dee08ec41a78ba8636bf64
SarvagnaTejaRoyal <sarvagnatejaroyal@gmail.com>
2025-07-12 21:16:20 +0530

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCy4w-b_Za_0ydgZu8JL9Xzvl0X3DOQg_I",
+  authDomain: "gym-management-system-83516.firebaseapp.com",
+  databaseURL: "https://gym-management-system-83516-default-rtdb.firebaseio.com",
+  projectId: "gym-management-system-83516",
+  storageBucket: "gym-management-system-83516.firebasestorage.app",
+  messagingSenderId: "715554105732",
+  appId: "1:715554105732:web:119ae450ac61cf008fd5d1",
+  measurementId: "G-VXF060NRVZ"
+};
+
+firebase.initializeApp(firebaseConfig);
